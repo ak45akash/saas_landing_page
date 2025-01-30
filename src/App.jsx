98e7ded7215be551ad7082a052656dd971bf8808
Akashdeep Kancha <ak45.akashdeep@gmail.com>
2025-01-30 16:30:36 +0530

    Added responsive header
    Implemented a fully responsive header using React and Tailwind CSS. Added smooth scrolling with react-scroll and conditional class management with clsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './sections/Header';
 
 const App = () => {
   return (
-      "App"
+      <main className='overflow-hidden'>
+        <Header />
+      </main>
   );
 };
 
