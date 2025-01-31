9ebe830f291037809f1f74ec9066315d964dac42
Akashdeep Kancha <ak45.akashdeep@gmail.com>
2025-01-31 18:14:05 +0530

    Added Download section
    Implemented the Download section to facilitate user access to the application. Enhanced the layout and responsiveness for better usability.

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -8,6 +8,7 @@ const Testimonials = () => {
 
     return <section className="relative z-2 py-24 md:py-28 lg:py-40">
         <div className="container block lg:flex">
+            {/* TODO:: Make this section sticky */}
             <div className="testimonials_head-res relative z-2 mr-20 flex-300">
                 <p className="caption mb-5 max-md:mb-2.5">Wall of Love</p>
                 <h3 className="h3 max-md:h5 text-p4">Words from our fans</h3>
